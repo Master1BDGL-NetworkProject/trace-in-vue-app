fa65725d633f7b8c31c860b2d98b551534aee279
ofceab <flutternow1963@gmail.com>
2022-02-10 01:05:16 +0000

    Setup vue app

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Hello there 🥰</h1>
+    <h3>I really suffer to setup this project ! Vue js + Neutralino Js</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
